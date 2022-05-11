51c28d45155ef3c48fca569ae47d92b8f8de27a6
Aung Kyaw Phyo (Sebastian Kein) <94116245+AungKyawPhyo1142@users.noreply.github.com>
2022-05-11 15:32:58 +0630

    Adding player's life UI

--- src/main/UI.java
+++ src/main/UI.java
@@ -1,6 +1,8 @@
 package main;
 
+import Object.OBJ_Heart;
 import Object.OBJ_Key;
+import Object.SuperObject;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
@@ -38,6 +40,9 @@ public class UI {
     // Dialogue texts
     public String currentDialogue;
     
+    // player status
+    BufferedImage heart_full,heart_half,heart_blank;
+    
     public UI(GamePanel gp){
             this.gp = gp;
 
@@ -50,6 +55,11 @@ public class UI {
             Logger.getLogger(UI.class.getName()).log(Level.SEVERE, null, ex);
         }
         
+        // create HUD Object
+        SuperObject heart = new OBJ_Heart(gp);
+        heart_full = heart.image;
+        heart_half = heart.image2;
+        heart_blank = heart.image3;
     }
     
     public void draw(Graphics2D g2){
@@ -66,17 +76,49 @@ public class UI {
         
         // IN PLAY STATE
         if(gp.gameState==gp.playState){
-            // do smth for the playstate
+            drawPlayerLife();
         }
         
         // IN PAUSE STATE
         if(gp.gameState==gp.pauseState){
             drawPauseScreen();
+            drawPlayerLife();
         }
         
         // IN DIALOGUE STATE
         if(gp.gameState==gp.dialogueState){
+            drawPlayerLife();
             drawDialogueScreen();
+        }
+        
+    }
+    
+    public void drawPlayerLife( ) {
+        int x = gp.tileSize/2;
+        int y = gp.tileSize/2;
+        int i = 0;
+        
+        // draw blank heart
+        while(i<gp.player.maxLife/2){
+            g2.drawImage(heart_blank, x, y,gp.tileSize,gp.tileSize,null);
+            i++;
+            x+=gp.tileSize;
+        }
+        
+        //reset
+        x = gp.tileSize/2;
+        y = gp.tileSize/2;
+        i = 0;
+        
+        //draw current life
+        while(i<gp.player.life){
+            g2.drawImage(heart_half, x, y,gp.tileSize,gp.tileSize,null);
+            i++;
+            if(i<gp.player.life){
+                g2.drawImage(heart_full, x,y,gp.tileSize,gp.tileSize,null);
+            }
+            i++;
+            x+=gp.tileSize;
         }
         
     }
